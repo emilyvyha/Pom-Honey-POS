42cd8ea75945e48004ee01f9037d1c1c4af1c153
emilyvyha120 <emilyvyha120@tamu.edu>
2022-12-02 17:05:37 -0600

    fixed the lowstockGUI to show items

--- frontend/src/Pages/LowstockGUI.js
+++ frontend/src/Pages/LowstockGUI.js
@@ -5,11 +5,11 @@ import { useState, useEffect, useContext } from "react";
 import axios from 'axios'
 import { Button, TextField } from "@mui/material"; 
 import { createTheme, ThemeProvider } from "@mui/material";
+import OneColRow from "../Components/OneColRow";
 
 // components
 import Header from "../Components/Header"
 import TranslatedText from "../Components/TranslatedText";
-import OneColRow from "../Components/OneColRow";
 
 // pages
 
@@ -23,41 +23,25 @@ const LowstockGUI = ()=> {
     const [isLoading, setIsLoading] = useState(false);
     const [err, setErr] = useState('');
 
-    const lowStock = async () => {
-        setIsLoading(true);
-        try {
-            const response = await fetch('http://localhost:5000/lowStock', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                },
-            });
-    
-        const result = await response.json();
-        setStockItems(result);
-        console.log(stockItems);
-        
-        } catch (err) {
-            setErr(err.message);
-        } finally {
-            setIsLoading(false);
-        }
-    }
+    useEffect(() => {
+        axios.get("http://localhost:5000/lowStock", {})
+          .then(data => {
+            setStockItems(data.data)
+          })
+        },[])
 
     return (
         <div style={{ height: "100%"}}>
             <Header title = "Low Stock" path = "/lowstockgui"/>
             <div style={{maxHeight:"1000px", overflowY:"scroll", border:"solid", borderWidth:2, borderColor:"blue", backgroundColor:"blue", marginTop:20, marginLeft: "20%", marginRight: "20%", fontSize: "30px"}}>
-
                 <div style={{borderBottom:'solid white 3px', position:"sticky",  top:0}}>
                     <OneColRow item = {"Items Low on Stock"} />
                 </div>
 
                 {(stockItems ?? []).map( (row) =>{
-                return (
-                    <OneColRow key = {row.id} item = {row}/>
-                )
+                    return (
+                        <OneColRow key = {row.id} item = {row}/>
+                    )
                 })}
 
             </div>
